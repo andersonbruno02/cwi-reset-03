e2d262a59cabbc84c06baf46467ce091b0870582
Anderson Bruno <andersonbruno02@hotmail.com>
2021-10-09 12:12:42 -0300

    Adicionado descrições na classe Genero

--- Aula 2/src/pacote/de/exercicios/Genero.java
+++ Aula 2/src/pacote/de/exercicios/Genero.java
@@ -1,7 +1,13 @@
 package pacote.de.exercicios;
 
 public enum Genero {
-    MASCULINO,
-    FEMININO,
-    NAO_BINARIO;
+    MASCULINO("É masculino o substantivo que admite o artigo \"o\"."),
+    FEMININO("É feminino o substantivo que admite o artigo \"a\"."),
+    NAO_BINARIO("O termo não-binário refere-se às pessoas que não se percebem como pertencentes a um gênero exclusivamente. Isso significa que sua identidade de gênero e expressão de gênero não são limitadas ao masculino e feminino.");
+
+    private String descricao;
+
+    Genero(String descricao) {
+        this.descricao = descricao;
+    }
 }
