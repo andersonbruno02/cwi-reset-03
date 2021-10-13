9081c865f28b8a0dc2a04eab450b30943a0833c3
Anderson Bruno <andersonbruno02@hotmail.com>
2021-10-12 22:09:13 -0300

    Criação do repositório desafio-resetflix

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/andersonbruno/AtorService.java
@@ -0,0 +1,13 @@
+package br.com.cwi.reset.andersonbruno;
+
+public class AtorService {
+
+    private FakeDatabase fakeDatabase;
+
+    public AtorService(FakeDatabase fakeDatabase) {
+        this.fakeDatabase = fakeDatabase;
+    }
+
+    // Demais métodos da classe
+
+}
